e0a77f4ef02598a05a29d356eb66e68982a13ecb
cchiu28 <scorpiusdx@gmail.com>
2022-11-30 01:00:56 -0500

    testing tailwindcss

--- src/Components/sidebar.tsx
+++ src/Components/sidebar.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function SideBar() {
 
     return(
-        <div>
-            test
+        <div className="relative min-h-screen flex">
+            <div className="bg-blue-800 text-blue-100">test</div>
         </div>
     )
 }
